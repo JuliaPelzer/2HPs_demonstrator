e5518d6e2f2594c14d1670c6c3253608a2c30808
Julia <jupelzer@outlook.de>
2023-09-06 09:14:28 +0200

    pipeline-apply2hpnn: only apply nn if second hp in reach AND prepare domain-dataset IN pipeline 2hpnn apply AN measure time

--- pipeline_apply.py
+++ pipeline_apply.py
@@ -16,6 +16,7 @@ sys.path.append("/home/pelzerja/pelzerja/test_nn/1HP_NN")  # relevant for remote
 sys.path.append("/home/pelzerja/Development/1HP_NN")  # relevant for local
 from data_stuff.utils import SettingsPrepare, load_yaml
 from networks.models import load_model
+from prepare_dataset import prepare_dataset
 from utils.utils import beep
 
 
@@ -36,11 +37,26 @@ def pipeline_apply_2HPNN(
     time_begin = time.perf_counter()
 
     # prepare large dataset if not done yet
-    (_, _, dataset_domain_path, _, model_1hp_path, model_2hp_path, datasets_prepared_2hp_dir, inputs_prep
+    (datasets_raw_domain_dir, datasets_prepared_domain_dir, domain_prepared_dir, datasets_model_trained_with_path, model_1hp_path, model_2hp_path, _, inputs_prep
      ) = set_paths(dataset_large_name, preparation_case, model_name_2hp=model_name_2HP,)
-    destination_2hp_prep = pathlib.Path(datasets_prepared_2hp_dir, f"{dataset_large_name}_2hp_test")
-    destination_2hp_prep.mkdir(parents=True, exist_ok=True)
-    destination_2hp_nn = pathlib.Path(os.getcwd(), "runs", destination_2hp_prep.name)
+    
+    if not os.path.exists(domain_prepared_dir):
+        args = SettingsPrepare(
+            raw_dir=datasets_raw_domain_dir,
+            datasets_dir=datasets_prepared_domain_dir,
+            dataset_name=dataset_large_name,
+            inputs_prep=inputs_prep,
+            # name_extension=name_extension,
+        )
+        prepare_dataset(args,
+            power2trafo=False,
+            info=load_yaml(datasets_model_trained_with_path, "info"),
+        )  # norm with data from dataset that NN was trained with!
+        print(f"Domain {domain_prepared_dir} prepared")
+    else:
+        print(f"Domain {domain_prepared_dir} already prepared")
+
+    destination_2hp_nn = pathlib.Path(os.getcwd(), "runs", domain_prepared_dir.name)
     destination_2hp_nn.mkdir(exist_ok=True)
 
     # load models
@@ -48,14 +64,13 @@ def pipeline_apply_2HPNN(
         {"model_choice": "unet", "in_channels": len(inputs_prep)}, model_1hp_path, "model", device)
     model_2HP = load_model(
         {"model_choice": "unet", "in_channels": 2}, model_2hp_path, "model", device)
-
-    check_all_datasets_prepared([dataset_domain_path, destination_2hp_prep])
     
     num_dp_valid = 0
     avg_time_prep_1hp = 0
     avg_time_apply_1nn = 0
     avg_time_prep_2hp = 0
     avg_time_apply_2nn = 0
+    avg_time_measure_and_visualize = 0
     num_hps_overall = 0
     avg_loss_mae = 0
     avg_loss_mse = 0
@@ -63,12 +78,14 @@ def pipeline_apply_2HPNN(
     avg_mse = {0: 0, 1: 0}
     num_split = {0: 0, 1: 0}
 
+    time_prepare = time.perf_counter() - time_begin
+
     # apply 1HP-NN and 2HP-NN
-    list_runs = os.listdir(os.path.join(dataset_domain_path, "Inputs"))
+    list_runs = os.listdir(os.path.join(domain_prepared_dir, "Inputs"))
     for run_file in tqdm(list_runs, desc="Apply 2HP-NN", total=len(list_runs)):
         run_id = f'{run_file.split(".")[0]}_'
         time_prep_1hp = time.perf_counter()
-        domain = Domain(dataset_domain_path, stitching_method="max", file_name=run_file)
+        domain = Domain(domain_prepared_dir, stitching_method="max", file_name=run_file)
         # generate 1hp-boxes and extract information like perm and ids etc.
         if domain.skip_datapoint:
             logging.warning(f"Skipping {run_id}")
@@ -100,18 +117,25 @@ def pipeline_apply_2HPNN(
             hp.interim_outputs = domain.norm(
                 hp.interim_outputs, property="Temperature [C]"
             )
-        avg_time_prep_2hp = time.perf_counter() - time_prep_2hp
+        avg_time_prep_2hp += time.perf_counter() - time_prep_2hp
+
 
         # apply 2HP-NN
         time_apply_2nn = time.perf_counter()
         for hp in single_hps:
-            hp.prediction_2HP = hp.apply_nn(model_2HP, inputs="interim_outputs")
-            hp.prediction_2HP = domain.reverse_norm(
-                hp.prediction_2HP, property="Temperature [C]"
-            )
-            domain.add_hp(hp, hp.prediction_2HP)
-        avg_time_apply_2nn = time.perf_counter() - time_apply_2nn
+            if not hp.interim_outputs.all() == 10.6:
+                hp.prediction_2HP = hp.apply_nn(model_2HP, inputs="interim_outputs")
+                hp.prediction_2HP = domain.reverse_norm(
+                    hp.prediction_2HP, property="Temperature [C]"
+                )
+                domain.add_hp(hp, hp.prediction_2HP)
+            else:
+                # if no overlap: dont apply 2HP-NN and just use 1HP-NN
+                domain.add_hp(hp, hp.prediction_1HP)
+                print("I didnt expect this to happen")
+        avg_time_apply_2nn += time.perf_counter() - time_apply_2nn
 
+        time_measure_and_visualize = time.perf_counter()
         for id_hp, hp in enumerate(single_hps):
             loss_mae, loss_mse = hp.measure_accuracy(domain, plot_args=[False, destination_2hp_nn / f"plot_hp{num_hps_overall}.png"])
             avg_mae[id_hp] += loss_mae
@@ -120,14 +144,16 @@ def pipeline_apply_2HPNN(
             avg_loss_mae += loss_mae
             avg_loss_mse += loss_mse
             num_hps_overall += 1
-        # domain.plot("goksit")
+        avg_time_measure_and_visualize += time.perf_counter() - time_measure_and_visualize
+        domain.plot("goksit")
         num_dp_valid += 1
     
     # avg measurements
     avg_time_prep_1hp /= num_dp_valid
     avg_time_apply_1nn /= num_dp_valid
-    avg_time_prep_2hp /= num_dp_valid
-    avg_time_apply_2nn /= num_dp_valid
+    avg_time_prep_2hp /= num_hps_overall
+    avg_time_apply_2nn /= num_hps_overall
+    avg_time_measure_and_visualize /= num_hps_overall
     avg_loss_mae /= num_hps_overall
     avg_loss_mse /= num_hps_overall
     for id_hp in avg_mae.keys():
@@ -142,10 +168,12 @@ def pipeline_apply_2HPNN(
                 "time whole process in sec": time.perf_counter() - time_begin,
                 "timestamp_begin": timestamp_begin,
                 "timestamp_end": time.ctime(),
-                "avg_time_prep_1hp in sec": avg_time_prep_1hp,
-                "avg_time_apply_1nn (incl. renorming) in sec": avg_time_apply_1nn,
-                "avg_time_prep_2hp (incl. norming) in sec": avg_time_prep_2hp,
-                "avg_time_apply_2nn (incl. renorming) in sec": avg_time_apply_2nn,
+                "avg_time_prep_1hp in sec (per dp)": avg_time_prep_1hp,
+                "avg_time_apply_1nn (incl. renorming) in sec (per dp)": avg_time_apply_1nn,
+                "avg_time_prep_2hp (incl. norming) in sec (per hp)": avg_time_prep_2hp,
+                "avg_time_apply_2nn (incl. renorming) in sec (per hp)": avg_time_apply_2nn,
+                "avg_time_measure_and_visualize in sec (per hp)": avg_time_measure_and_visualize,
+                "time to prepare paths etc. in sec": time_prepare,
                 "number valid datapoints": num_dp_valid,
                 "avg_loss_mae": float(avg_loss_mae),
                 "avg_loss_mse": float(avg_loss_mse),
@@ -167,6 +195,8 @@ def pipeline_apply_2HPNN(
             },
             file,
         )
+
+    logging.warning(f"Finished applying 2HP-NN to {num_dp_valid} datapoints, results are at {destination_2hp_nn}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
